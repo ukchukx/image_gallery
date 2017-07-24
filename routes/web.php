56c135f315ce2c84091df9aa79aeb91e5b7eb96b
Uk Chukundah <ukchukundah@gmail.com>
2017-07-24 21:59:40 +0100

    Remove extraenous text.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,4 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 Route::get('/', 'ImageGalleryController@index');
 Route::post('/', 'ImageGalleryController@upload');
 Route::delete('/{id}', 'ImageGalleryController@destroy');
-// Route::get('/', function() {
-//   return view('welcome');
-// });
